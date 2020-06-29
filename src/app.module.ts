abcd3a8e8b22c7067e94a0c556d572afe1e588cc
Renan Reboredo <renanrreboredo@gmail.com>
2020-06-28 22:43:17 -0300

    Configure static files rendering for project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
+import { InvestmentModule } from './investment/investment.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb+srv://root:${ process.env.DATABASE_PASSWORD }@fuzzytrader.oaqab.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority`)
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'app'),
+    }),
+    InvestmentModule,
+    MongooseModule.forRoot(`mongodb+srv://root:${process.env.DATABASE_PASSWORD}@fuzzytrader.oaqab.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority`)
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
