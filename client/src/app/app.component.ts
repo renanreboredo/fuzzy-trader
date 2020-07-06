f858de4f983379ec1b499dc3b84e089410b659ce
Renan Reboredo <renanrreboredo@gmail.com>
2020-07-06 19:51:55 -0300

    Add base layout for trader client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,11 +11,32 @@ import { Response } from '../../../src/domain/response';
 export class AppComponent {
   title = 'Fuzzy Trader';
   amount: number;
-  offers$: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  conservative$: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  moderate$: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  aggressive$: BehaviorSubject<any> = new BehaviorSubject<any>({});
   constructor(private http: HttpClient) {}
   trade(): any {
-    this.http.get(`https://bxblue-fuzzy-trader.herokuapp.com/api/investments/${this.amount}`).subscribe(
-      (res: Response<any>) => this.offers$.next(res.data)
+    this.http.get(`https://bxblue-fuzzy-trader.herokuapp.com/api/investments/${this.amount}/simulate`).subscribe(
+      ({ data }: Response<any>) => {
+        this.conservative$.next({
+          buyingPrice: data.conservative.buyingPrice,
+          quantity: data.conservative.quantity,
+          symbol: data.conservative.symbol,
+          total: data.conservative.buyingPrice * data.conservative.quantity
+        });
+        this.moderate$.next({
+          buyingPrice: data.moderate.buyingPrice,
+          quantity: data.moderate.quantity,
+          symbol: data.moderate.symbol,
+          total: data.moderate.buyingPrice * data.moderate.quantity
+        });
+        this.aggressive$.next({
+          buyingPrice: data.aggressive.buyingPrice,
+          quantity: data.aggressive.quantity,
+          symbol: data.aggressive.symbol,
+          total: data.aggressive.buyingPrice * data.aggressive.quantity
+        });
+      }
     );
   }
 }
