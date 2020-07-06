4cee8811ac18f8eafa5f21bd8fec19fd752774f8
Renan Reboredo <renanrreboredo@gmail.com>
2020-07-06 04:15:26 -0300

    Add user creation in user service

--- src/domain/schemas/financial-asset.schema.ts
+++ src/domain/schemas/financial-asset.schema.ts
@@ -1,9 +1,9 @@
 import { Schema } from 'mongoose';
 
 export const FinancialAssetsSchema = new Schema({
-  // user_id: Number,
   type: String,
   symbol: String,
   buyingPrice: Number,
   quantity: Number,
+  user_id: Number,
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../domain/user';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async create(user: User) {
+    const createdUser = new this.userModel(user);
+    return await createdUser.save();
+  }
+}
